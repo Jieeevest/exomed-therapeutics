fcb7e12684c3b3448e772e8ae2e9c83d8141f641
Muhamad Firli Ismail <m.firliismail@gmail.com>
2026-05-23 07:01:56 +0700

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ export default {
   content: ['./index.html', './src/**/*.{ts,tsx}'],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Inter', 'system-ui', 'sans-serif'],
+      },
       colors: {
         background: 'hsl(var(--background))',
         foreground: 'hsl(var(--foreground))',
